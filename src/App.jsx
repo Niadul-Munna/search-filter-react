d1e387a42875063583f869aa344a5623ae3cf22f
niadulMunna <niadulislammunna@gmail.com>
2025-03-14 14:30:22 +0600

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { FilterItem } from "./components/filter-item";
 function App() {
   return (
     <>
-      <div className="bg-gradient-to-r from-blue-100 to-purple-100 min-h-screen "></div>
+      <div className="bg-gradient-to-r from-blue-100 to-purple-100 min-h-screen flex flex-col items-center w-screen  container mx-auto">
+        <FilterItem />
+      </div>
     </>
   );
 }
