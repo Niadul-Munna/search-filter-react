ce230df50b45c6c11c403d937a1afeccc3c4eba1
niadulMunna <niadulislammunna@gmail.com>
2025-03-14 14:31:10 +0600

    default filter-item created

---
+++ src/components/filter-item.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const FilterItem = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Search here..." />
+      <button className="">Search</button>
+    </div>
+  );
+};
