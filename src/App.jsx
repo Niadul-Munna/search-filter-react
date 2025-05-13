cafaf1b6db59cb3cf40c5853f0a4fab1eb939479
niadul-munna <niadulislammunna@gmail.com>
2025-05-13 23:51:40 +0600

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { FilterItem } from "./components/filter-item";
+import Navbar from "./components/navbar";
 function App() {
   return (
     <>
+      <Navbar />
       <div className="bg-gradient-to-r from-blue-100 to-purple-100 min-h-screen flex flex-col items-center">
         <FilterItem />
       </div>
